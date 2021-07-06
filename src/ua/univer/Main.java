3f676c708e343c47f11226b113f074dc79877c90
mmveres <mmveres@gmail.com>
2021-07-06 21:49:47 +0300

    lesson01

---
+++ src/ua/univer/Main.java
@@ -0,0 +1,22 @@
+package ua.univer;
+
+public class Main {
+    public static int task1(int n1) {
+
+        if (n1 == 0) {
+            return 0;
+        } else if (n1 > 0) {
+            return n1 * n1;
+        } else return Math.abs(n1);
+    }
+
+    public static void main(String[] args) {
+
+     //   System.out.println("Hello people");
+        int expResult = 0;
+        int actResult = task1(0);
+     //   System.out.print(expResult == actResult);
+        int x =123;
+        System.out.print(x%100);
+    }
+}
